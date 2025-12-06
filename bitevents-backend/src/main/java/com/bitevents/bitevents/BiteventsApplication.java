d8e277998e26e2858e3a6071858b90afe85a794f
Lubomir Becka <lubomir.beckaml@gmail.com>
2025-12-06 23:23:16 +0100

    Backend initialization

---
+++ bitevents-backend/src/main/java/com/bitevents/bitevents/BiteventsApplication.java
@@ -0,0 +1,13 @@
+package com.bitevents.bitevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteventsApplication.class, args);
+	}
+
+}
---
+++ bitevents-backend/src/test/java/com/bitevents/bitevents/BiteventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitevents.bitevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
