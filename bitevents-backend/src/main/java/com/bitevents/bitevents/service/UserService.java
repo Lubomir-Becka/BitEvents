dcc30be299f2dbe65e87de01736980ff91d1de61
Lubomir Becka <lubomir.beckaml@gmail.com>
2025-12-07 00:20:43 +0100

    Add user service prototype

--- bitevents-backend/src/main/java/com/bitevents/bitevents/service/UserService.java
+++ bitevents-backend/src/main/java/com/bitevents/bitevents/service/UserService.java
@@ -36,7 +36,7 @@ public class UserService {
 
         return userRepository.save(user);
     }
-    
+
     public User findByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("Používateľ nebol nájdený."));
