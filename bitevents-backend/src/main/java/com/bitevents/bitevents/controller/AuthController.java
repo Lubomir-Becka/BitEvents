8c4238fd999abb38e97df14a2e3bea9c0f5168b2
Lubomir Becka <lubomir.beckaml@gmail.com>
2025-12-07 01:05:55 +0100

    Add auth controller

---
+++ bitevents-backend/src/main/java/com/bitevents/bitevents/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.bitevents.bitevents.controller;
+
+import com.bitevents.bitevents.dto.RegistrationRequest;
+import com.bitevents.bitevents.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegistrationRequest request) {
+        try {
+            userService.registerNewUser(request);
+            return new ResponseEntity<>("Používateľ úspešne zaregistrovaný.", HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Chyba pri registrácii: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
