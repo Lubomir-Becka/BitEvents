b18db769699ec5aa21456a0ae124300b3f1a8cdc
lubomir-becka <lubomir.beckaml@gmail.com>
2026-01-18 18:42:48 +0100

    Fix gradient class syntax in register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -275,7 +275,7 @@ export const Register: React.FC = () => {
       </div>
 
       {/* RIGHT COLUMN - EMPTY GRADIENT */}
-      <div className="hidden lg:flex lg:w-1/2 bg-linear-to-br from-gray-800 to-gray-900"></div>
+      <div className="hidden lg:flex lg:w-1/2 bg-gradient-to-br from-gray-800 to-gray-900"></div>
     </div>
   );
 };
